4d9d2adbbfc9697584ce74f963dc5dcd6238dc49
bthomas2622 <benthomasdeveloper@gmail.com>
2023-11-14 14:43:37 -0800

    category wordlists and esmodules over commonjs

--- scripts/compile-wordlists.js
+++ scripts/compile-wordlists.js
@@ -1,4 +1,9 @@
 import { readdirSync, readFileSync, writeFileSync } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const wordlistsFilePath = '/category-wordlists/'
 const wordlists = readdirSync(__dirname + `/..${wordlistsFilePath}`);
--- tests/test-wordlist.js
+++ tests/test-wordlist.js
@@ -1,4 +1,9 @@
 import { readdirSync, readFile } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const wordlistsFilePath = '/category-wordlists'
 const wordlists = readdirSync(__dirname + `/..${wordlistsFilePath}`);
